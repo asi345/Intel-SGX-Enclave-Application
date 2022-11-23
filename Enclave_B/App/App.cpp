6219701aac7d3f7c74f20a80387930d7628e5029
asi345 <inanata15@gmail.com>
2022-11-23 22:37:19 +0100

    paranoic

--- Enclave_B/App/App.cpp
+++ Enclave_B/App/App.cpp
@@ -246,8 +246,6 @@ int SGX_CDECL main(int argc, char *argv[])
         return -1;
     }
     printf("From App: Enclave creation success. \n");  
-    printf("From App: Write your protocol here ... \n");
-
 
     sgx_status_t sgx_status;
     sgx_status_t ret;
--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ touch /tmp/fifoA /tmp/fifoB /tmp/fifoA2 /tmp/fifoB2 /tmp/fifoA3 /tmp/fifoB3
 cd Enclave_A
 make clean
 make SGX_MODE=SIM
-./app > App_A_output.txt &
+./app > ../App_A_output.txt &
 
 cd ../Enclave_B
 make clean
 make SGX_MODE=SIM
-./app > App_B_output.txt &
+./app > ../App_B_output.txt &
